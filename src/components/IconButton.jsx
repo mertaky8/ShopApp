fb47d74b4aa2bace36c4caea34017193d83e8895
Mert Akkaya <76126526+mertaky8@users.noreply.github.com>
2024-07-26 20:16:47 +0200

    comments

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
 const IconButton = ({ handleClick }) => {
   return (
-    <button
+    <Button
       onClick={(e) => {
-        e.stopPropagation();
+        e.stopPropagation(); // parent child ilişkisine göre tüm rowdan değil sadece silme butonuna tıklanıldığında işlem gerçekleşmesi sağlanır.
         handleClick();
       }}
     >
       <FaTrash />
-    </button>
+    </Button>
   );
 };
 
